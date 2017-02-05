aa806a353d9cf1c9ccbfd24c2bee27c36449da4f
Ivan Ivanov <i.ivanov.it@gmail.com>
2017-02-05 23:46:02 +0200

    Refactor teampulse api
    Refactor teampulse item to ES6 and to use request-promise. Request-promise is module wrapper around request library which works with promises. Also it check response statuses and will throw error if status is not 2xx.

--- teampulse/teampulse.js
+++ teampulse/teampulse.js
@@ -1,103 +1,59 @@
-var request = require('request');
-var httpntlm = require('httpntlm');
-var queryString = require('query-string');
-var Promise = require('bluebird')
+const request = require('request'),
+      httpntlm = require('httpntlm'),
+      Promise = require('bluebird'),
+      httpntlmGet = Promise.promisify(httpntlm.get),
+      queryString = require('query-string'),
+      requestPromise = require('request-promise');
 
-module.exports = function(config) {
-    config = config || {}
-    config.url = config.url || 'http://teampulse.com'
-    this.config = config;
+class Teampulse {
+    constructor(config) {
+        config = config || {}
+        config.url = config.url || 'http://teampulse.com'
+        this.config = config;
 
-    this.authenticate = function(credentials) {
-        var that = this;
-
-        return new Promise(function (resolve, reject) {
-            httpntlm.get({
-                url: that.config.url + '/WinLogin/Login.aspx?ReturnUrl=0',
-                username: credentials.username,
-                password: credentials.password,
-                domain:  credentials.domain
-            }, function (err, res){
-                if(err) {
-                    reject(err);
-                }
-
-                authCookieValue = that._getAuthCookie(res);
-
-                request({
-                    url: that.config.url + '/Authenticate/WRAPv0.9?wrap_client_id=uri%3aTeamPulse',
-                    headers: {
-                        'Cookie': that.consts.ASP_COOKIE + authCookieValue
-                    }
-                }, function (error, response, body) {
-                    if (!error && response.statusCode === 200) {
-                        var verificationCode = that._getVerificationCode(body);
-                        request({
-                            url: that.config.url + '/Authenticate/WRAPv0.9?wrap_client_id=uri%3aTeamPulse',
-                            headers: {
-                                'Content-Type': 'application/x-www-form-urlencoded'
-                            },
-                            method: 'POST',
-                            body: 'wrap_client_id=uri:TeamPulse&wrap_verification_code=' + verificationCode,
-                        }, function (error, response, body) {
-                            var parsed = queryString.parse(body),
-                                accessToken = parsed.wrap_access_token;
-
-                            that.wrap_access_token = parsed.wrap_access_token;
-                            resolve();
-                        })
-                    }
-                });
-            });
-        });
+        this.consts = {
+            ASP_COOKIE: '.ASPXAUTH='
+        };
     }
 
-    this.getItem = function(id) {
-        var that = this;
-        return new Promise(function (resolve, reject) {
-            request({ 
-                url: that.config.url + '/api/workitems/' + id,
-                headers: {
-                            'Authorization': 'WRAP access_token="' + that.wrap_access_token +'"',
-                            'Content-Type': 'application/json',
-                            'Accept': '*/*'
-                        },
-                method: 'GET'
-            }, function (error, response, body) {
-                if (!error && response.statusCode === 200) {
-                    resolve(JSON.parse(body));
-                    return;
-                }
-                
-                reject(error);      
-            });
-        });
+    authenticate(credentials) {
+        return this._getAuthCookie(credentials)
+                   .then(this._getVerificationCode.bind(this))
+                   .then(this._getAccessToken.bind(this))
+                   .then(accessToken => this.wrap_access_token = accessToken);
     }
 
-    this.putItem = function(id, item) {
-        var that = this;
-        return new Promise(function (resolve, reject) {
-            request({ 
-                url: that.config.url + '/api/workitems/' + id,
-                headers: {
-                            'Authorization': 'WRAP access_token="' + that.wrap_access_token +'"',
-                            'Content-Type': 'application/json',
-                            'Accept': '*/*'
-                        },
-                method: 'PUT',
-                json: item
-            }, function (error, response, body) {
-                if (!error && response.statusCode === 200) {
-                    resolve();
-                    return;
-                }
-                
-                reject(error);      
-            });
-        });
+    getItem(id) {
+        const getItemOptions = { 
+            url: `${this.config.url}/api/workitems/${id}`,
+            headers: {
+                'Authorization': `WRAP access_token="${this.wrap_access_token}"`,
+                'Content-Type': 'application/json',
+                'Accept': '*/*'
+            },
+            method: 'GET',
+            json: true
+        };
+
+        return requestPromise(getItemOptions);
     }
 
-    this.changeStatus = function(itemId, status) {
+    putItem(id, item) {
+        const putItemOptions = { 
+            url:  `${this.config.url}/api/workitems/${id}`,
+            headers: {
+                'Authorization': `WRAP access_token="${this.wrap_access_token}"`,
+                'Content-Type': 'application/json',
+                'Accept': '*/*'
+            },
+            method: 'PUT',
+            json: item
+        };
+
+        return requestPromise(putItemOptions);
+    }
+
+    changeStatus(itemId, status) {
 		const propertiesToChange = { 
 			"Status":  status 
 		};
@@ -105,8 +61,43 @@ module.exports = function(config) {
 		return this.putItem(itemId, propertiesToChange);
 	}
 
-    this._getAuthCookie = function(response) {
-        var setCookieHeaders = response.headers['set-cookie'],
+    _getAuthCookie(credentials) {
+        const getAuthCookieOptions = {
+            url: `${this.config.url}/WinLogin/Login.aspx?ReturnUrl=0`,
+            username: credentials.username,
+            password: credentials.password,
+            domain:  credentials.domain
+        };
+
+        return httpntlmGet(getAuthCookieOptions).then(this._extractAuthCookie.bind(this));
+    }
+
+    _getVerificationCode(authCookieValue) {
+        const getVerificationCodeOptions = { 
+            url: `${this.config.url}/Authenticate/WRAPv0.9?wrap_client_id=uri%3aTeamPulse`,
+            headers: {
+                'Cookie':`${this.consts.ASP_COOKIE}${authCookieValue}`
+            }
+        };
+        
+        return requestPromise(getVerificationCodeOptions).then(this._extractVerificationCode.bind(this));
+    }
+
+    _getAccessToken(verificationCode) {
+        const getAccessTokenOptions = { 
+            url: `${this.config.url}/Authenticate/WRAPv0.9?wrap_client_id=uri%3aTeamPulse`,
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            },
+            method: 'POST',
+            body: `wrap_client_id=uri:TeamPulse&wrap_verification_code=${verificationCode}`,
+        };
+
+        return requestPromise(getAccessTokenOptions).then(response => queryString.parse(response).wrap_access_token);
+    }
+
+    _extractAuthCookie(response) {
+        let setCookieHeaders = response.headers['set-cookie'],
             header,
             authCookieStartIndex,
             authCookieEndIndex;
@@ -121,16 +112,14 @@ module.exports = function(config) {
         }
     }
 
-    this._getVerificationCode = function(body) {
-        var verificationCodeRegex = /<title>Authentication Delegation, code=(.+)<\/title>/,
-            verificationCodeMatch = body.match(verificationCodeRegex);
+    _extractVerificationCode(body) {
+        const verificationCodeRegex = /<title>Authentication Delegation, code=(.+)<\/title>/,
+              verificationCodeMatch = body.match(verificationCodeRegex);
         
         if(verificationCodeMatch != null) {
             return verificationCodeMatch[1];
         }
     }
-
-    this.consts = {
-        ASP_COOKIE: '.ASPXAUTH='
-    }
 }
+
+module.exports = Teampulse;
